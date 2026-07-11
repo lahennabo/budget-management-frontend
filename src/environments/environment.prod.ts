9dee2d1969748f9b661025b98db85f1c52013027
lah.ennabo <lah.ennabo@gmail.com>
2026-07-11 22:12:12 +0200

    correction du chemin du backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://railway.app' // Votre URL Railway ici
+  apiUrl: 'https://backend-budget-management-production.up.railway.app' // Votre URL Railway ici
 };
